001f24178ba8ff70a7caf797beaa590cc859707a
Ferrari248F1 <ferrarif07@gmail.com>
2016-03-07 14:52:57 +0100

    +Added something

--- pump_site/pump_app/REST_classes/CourseSerializer.py
+++ pump_site/pump_app/REST_classes/CourseSerializer.py
@@ -1,9 +1,15 @@
 from pump_app.model_classes.Course import Course
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class CourseSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Course
-		fields = ('id', 'name', 'description', 'open', 'startDate', 'endDate', 'coursecatalog')
+
+		user = User.objects.get(username='federico')
+		if user.has_perm('pump_app.change_course'):
+			fields = ('id', 'name', 'description', 'open', 'startDate', 'endDate', 'coursecatalog')
+		else:
+			fields = ('id', 'name', 'description', 'open', 'startDate', 'endDate')
