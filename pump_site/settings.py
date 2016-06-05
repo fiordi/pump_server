9715172efe229c13ce85510aaf9a2959e36bf26f
Ferrari248F1 <ferrarif07@gmail.com>
2016-06-05 15:36:37 +0200

    +Fixed issues in static collect

--- pump_site/settings.py
+++ pump_site/settings.py
@@ -162,4 +162,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+ './static',
+)
+
 
